4e06ba382989a6b5f4708aef81f812b285b26a47
Kenneth.Wang <785172550@qq.com>
2019-07-24 17:28:43 +0800

    Create DelayQDemo.java

---
+++ src/main/java/manage/DelayQDemo.java
@@ -0,0 +1,10 @@
+package manage;
+
+import kafka.utils.KafkaScheduler;
+
+public class DelayQDemo {
+  public static void main(String[] args) {
+    KafkaScheduler scheduler; // based on kafka server jar
+    // to do
+  }
+}
